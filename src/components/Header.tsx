2d14aec0de913ce96b0d92a096609d3898f2e622
Murillou <Murillovinicius13@hotmail.com>
2024-05-18 03:28:56 -0300

    [feat] criando e estruturando component header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header className="flex  justify-between font-sans p-5 max-w-7xl mx-auto ">
+      <div>DevMori</div>
+      <ul className="flex gap-3">
+        <li>Sobre mim</li>
+        <li>Projetos</li>
+        <li>Skills</li>
+        <li>Contato</li>
+      </ul>
+    </header>
+  );
+}
