9c7e781915b7f5a025b96c26d72bddd61d38e2ea
Murillou <Murillovinicius13@hotmail.com>
2024-05-18 03:47:43 -0300

    [feat] adicionando estilização na aplicação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="flex  justify-between font-sans p-5 max-w-7xl mx-auto ">
+    <header className="flex flex-col justify-between font-poppins p-5 max-w-screen-2xl mx-auto bg-gray text-gray-text ">
       <div>DevMori</div>
       <ul className="flex gap-3">
         <li>Sobre mim</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+
+      colors: {
+        gray: '#1c1a1a',
+        'gray-text': '#E9E9E8',
       },
     },
   },
