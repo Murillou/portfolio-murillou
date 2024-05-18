6b04aa4813bb0f757a819bd53c395fe919b81e5e
Murillou <Murillovinicius13@hotmail.com>
2024-05-18 04:20:52 -0300

    [feat] criação e estilização do component perfil

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export function Header() {
   return (
-    <header className="flex flex-col justify-between font-poppins p-5 max-w-screen-2xl mx-auto bg-gray text-gray-text ">
+    <header className="flex flex-col justify-between font-poppins p-10 max-w-screen-2xl mx-auto bg-gray text-gray-text md:flex-row">
       <div>DevMori</div>
-      <ul className="flex gap-3">
+      <ul className="flex gap-10">
         <li>Sobre mim</li>
         <li>Projetos</li>
         <li>Skills</li>
---
+++ src/components/Perfil/Perfil.tsx
@@ -0,0 +1,41 @@
+import { GithubLogo, InstagramLogo, LinkedinLogo } from 'phosphor-react';
+import perfilImage from '../../assets/minha-foto.jpeg';
+
+export function Perfil() {
+  return (
+    <main className="flex items-center gap-40 max-w-screen-2xl mx-auto p-10">
+      <section>
+        <div>
+          <img
+            className="rounded-full border-2 border-solid border-red-900 "
+            src={perfilImage}
+            alt=""
+          />
+        </div>
+      </section>
+
+      <section className="space-y-5">
+        <h1 className="font-extrabold font-poppins text-4xl text-gray-text ">
+          Olá, me chamo Murillo Vinícius! :D
+        </h1>
+        <p className="font-semibold font-mono text-2xl text-slate-100">
+          Desenvolvedor Front-End
+        </p>
+        <div className="flex gap-3">
+          <a
+            href="https://www.linkedin.com/in/murillo-vin%C3%ADcius-ferreira-pontes-5a7595219/"
+            target="_blank"
+          >
+            <LinkedinLogo className="text-blue-500" size={50} />
+          </a>
+          <a href="https://www.github.com/Murillou" target="_blank">
+            <GithubLogo className="text-slate-100" size={50} />
+          </a>
+          <a href="https://www.instagram.com/moori_v/" target="_blank">
+            <InstagramLogo className="text-orange-500" size={50} />
+          </a>
+        </div>
+      </section>
+    </main>
+  );
+}
