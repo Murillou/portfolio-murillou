89014bc0d10597f0933b062c11320eadd7baef29
Murillou <Murillovinicius13@hotmail.com>
2025-02-03 03:23:38 -0300

    feat adicionando mais skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,8 +1,9 @@
 import { SiTypescript, SiTailwindcss, SiVim } from 'react-icons/si';
-import { FaReact } from 'react-icons/fa';
+import { FaReact, FaBootstrap } from 'react-icons/fa';
 import { MdHtml, MdCss } from 'react-icons/md';
-import { FaNode, FaSass } from 'react-icons/fa6';
+import { FaNode, FaSass, FaGitAlt } from 'react-icons/fa6';
 import { AiOutlineConsoleSql } from 'react-icons/ai';
+import { SiStyledcomponents, SiNextdotjs } from 'react-icons/si';
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -19,10 +20,18 @@ export function Skills() {
   }, []);
 
   const skills = [
-    { icon: MdHtml, color: 'text-orange-400', name: 'HTML' },
-    { icon: MdCss, color: 'text-blue-400', name: 'CSS' },
-    { icon: SiTypescript, color: 'text-blue-500', name: 'TypeScript' },
+    { icon: SiNextdotjs, color: 'text-white', name: 'Next.js' },
     { icon: FaReact, color: 'text-blue-400', name: 'React' },
+    { icon: SiTypescript, color: 'text-blue-500', name: 'TypeScript' },
+    { icon: MdCss, color: 'text-blue-400', name: 'CSS' },
+    { icon: MdHtml, color: 'text-orange-400', name: 'HTML' },
+    { icon: FaBootstrap, color: 'text-purple-400', name: 'Bootstrap' },
+    { icon: FaGitAlt, color: '', name: 'Git' },
+    {
+      icon: SiStyledcomponents,
+      color: 'text-yellow-400',
+      name: 'Styled Components',
+    },
     { icon: FaNode, color: 'text-green-800', name: 'NodeJs' },
     { icon: SiTailwindcss, color: 'text-teal-400', name: 'TailWind CSS' },
     { icon: FaSass, color: 'text-pink-400', name: 'Sass' },
